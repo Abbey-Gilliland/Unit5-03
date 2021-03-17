10d1dfcbe8ce762ab96e03c2427ad4c08281528f
Abbey Gilliland <abbey.gilliland@ocsbstudent.ca>
2021-03-17 13:43:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//this code makes the submit button function. It takes the value of the textbox and makes it into an alert.
+document.getElementById('submit-button').addEventListener('click', myfunction)
+
+function myfunction () {
+  alert(document.getElementById('message').value)
+}
